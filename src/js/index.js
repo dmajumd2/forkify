68da8508cf8e16cb04517b95e949ccf9c52911d0
Debashish <dmajumd2@binghamton.edu>
2018-05-26 18:06:53 -0400

    Added Code for searching the result and display the list of search item

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,31 @@
 import Search from './models/Search'
+import * as searchView from './views/searchView';
+import { elements } from './views/base';
 
-const search = new Search('pizza');
-console.log(search);
-search.getResults();
+const state = {};
+
+const controlSearch = async () => {
+    //get query from view
+    const query = searchView.getInput(); // we get input from the search text
+
+    if(query){
+        // new search object and add to state
+        state.search = new Search(query);  // we make a search class object for using Search.js and getting a result from the api
+
+        //prepare UI for results
+        searchView.clearInput();
+        searchView.clearPreviousResult();
+
+        //search for recipes
+       await state.search.getResults(); // using that object we get the result what to search
+
+        // render result in UI
+        searchView.renderResults(state.search.result); // render the search result
+    } 
+}
+elements.searchForm.addEventListener('submit', e => {
+    e.preventDefault();
+    controlSearch();
+});
+
+
